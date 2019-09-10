0033d13acbd1b7c904d638cadf494f1f6b926ff3
jullienfall <jullien.fall@gmail.com>
2019-09-10 13:13:31 +0300

    init

---
+++ src/context/BlogContext.js
@@ -0,0 +1,26 @@
+import createDataContext from '../context/createDataContext';
+
+const blogReducer = (state, action) => {
+  switch(action.type) {
+    case 'ADD_BLOGPOST': 
+      return [...state, { id: Math.floor(Math.random() * 99999), title: `Blog Post #${state.length + 1}` }];
+    case 'DELETE_BLOGPOST': 
+      return state.filter(blogPost => blogPost.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
+const addBlogPost = dispatch => {
+  return () => {
+    dispatch({ type: 'ADD_BLOGPOST' });
+  };
+};
+
+const deleteBlogPost = dispatch => {
+  return (id) => {
+    dispatch({ type: 'DELETE_BLOGPOST', payload: id })
+  };
+};
+
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost }, []);
---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,51 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+;
+
+import { Context} from '../context/BlogContext';
+
+const IndexScreen = () => {
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context);
+
+  return (
+    <View>
+      <Button title="Add Post" onPress={addBlogPost} />
+      {state ? 
+        <FlatList
+          data={state}
+          keyExtractor={blogPost => blogPost.title}
+          renderItem={({item}) => {
+            return (
+              <View style={styles.row}>
+                <Text style={styles.title}>{item.title} - {item.id}</Text>
+                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                  <Feather style={styles.icon} name="trash" />
+                </TouchableOpacity>
+              </View>
+            )
+          }}
+        /> : null 
+      }
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: 'grey',
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+  }
+});
+
+export default IndexScreen;
